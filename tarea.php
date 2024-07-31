e64994ea625bc61f8026c2c1d563237d66143107
CarBe03 <carolopintag2003@hotmail.com>
2024-07-30 19:34:21 -0500

    Tarea

--- tarea.php
+++ tarea.php
@@ -1,4 +1,26 @@
 <?php
 
+//Integeer
+$a=2;
+//Float
+$b=2.12;
+//String
+$c="Carlos";
+//Boolean
+$d=false;
+//Array
+$e=array("amarillo", "azul", "rojo");
+
+//Imprimir los valores para comporbar si esta bien 
+
+echo "Integer:".$a."<br>";
+
+echo "Float:".$b."<br>";
+
+echo "String:".$c."<br>";
+    
+echo "Boolean:".($d ? 'true':'false')."<br>";
+    
+echo "Array:".implode(", ", $e)."<br>";
 
 ?>
