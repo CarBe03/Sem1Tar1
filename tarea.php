7935fafac8a4d310cdc83704c5da3d67709b6bf1
CarBe03 <carolopintag2003@hotmail.com>
2024-08-06 19:15:53 -0500

    Tarea

--- tarea.php
+++ tarea.php
@@ -2,18 +2,45 @@
 
 //Integeer
 $a=2;
+$a1=9;
+$sum=$a1+$a;
+$mul=$a*$a1;
 //Float
 $b=2.12;
 //String
 $c="Carlos";
 //Boolean
 $d=false;
+// Estructura de control condicional
+if ($d) {
+    echo "La variable es verdadera.\n";
+} else {
+    echo "La variable es falsa.\n";
+}
 //Array
 $e=array("amarillo", "azul", "rojo");
 
-//Imprimir los valores para comporbar si esta bien 
+// Crear una variable de tipo cadena
+$ca1 = "Bienvenido, ";
+$ca2 = "Carlos!";
+
+// Concatenación de dos cadenas
+$cadenaconcatenada = $ca1 . $ca2;
+
+// Obtener la longitud de la cadena concatenada
+$longitud = strlen($cadenaconcatenada);
+
+// Mostrar resultados
+
+echo "Cadena concatenada: " . $cadenaconcatenada . "\n";
+
+echo "Longitud de la cadena concatenada: " . $longitud . "\n";
 
 echo "Integer:".$a."<br>";
+
+echo "Suma:".$sum."<br>";
+
+echo "Multiplicación:".$mul."<br>";
 
 echo "Float:".$b."<br>";
 
